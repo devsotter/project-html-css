a62597e4c1ed842767f187d2929de9623b20a2c3
Nicholas Sotter <112256391+devsotter@users.noreply.github.com>
2022-10-04 21:22:44 -0300

    updates

--- script.js
+++
@@ -1,9 +0,0 @@
-/*Menu Hamburger*/
-
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburguer.addEventListener("click", () => {
-  hamburguer.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
