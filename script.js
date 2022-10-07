ce37528b6ac4756c520015172cfd75b7e6ee720b
Nicholas Sotter <112256391+devsotter@users.noreply.github.com>
2022-10-06 23:09:17 -0300

    finalizado para teste

--- script.js
+++ script.js
@@ -67,5 +67,36 @@ class SlideStories {
     this.addNavigation();
   }
 }
-
 new SlideStories("slide");
+
+ScrollReveal({ reset: true });
+ScrollReveal().reveal(".area-1", {
+  delay: 500,
+  rotate: {
+    x: 0,
+    y: 100,
+    z: 0,
+  },
+});
+ScrollReveal().reveal(".area-2", {
+  delay: 500,
+});
+ScrollReveal().reveal(".area-3", {
+  delay: 500,
+  rotate: {
+    x: 0,
+    y: 100,
+    z: 0,
+  },
+});
+ScrollReveal().reveal(".area-4", {
+  delay: 500,
+});
+ScrollReveal().reveal(".area-5", {
+  delay: 500,
+  rotate: {
+    x: 0,
+    y: 100,
+    z: 0,
+  },
+});
